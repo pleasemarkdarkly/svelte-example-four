448338e7d00722fb0ac7c8ff6b22ad2ab50827c2
Mark Phillips <me@compuglobalhypermegacorp.com>
2021-02-04 23:31:03 -0800

    update

--- src/fireapp/fireapp.ts
+++ src/fireapp/fireapp.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-import { config } from './config';
+import config from './config';
 
 export const fireapp = initializeApp(config);
 export const auth = getAuth(fireapp);
